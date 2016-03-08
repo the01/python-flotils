3390583eb37924b4a4b3212b87c5a2db027b3208
d01 <jungflor@gmail.com>
2016-03-08 11:44:06 +0100

    Moved webscraper to own package floscraper

--- flotils/__init__.py
+++ flotils/__init__.py
@@ -7,17 +7,15 @@ __author__ = "the01"
 __email__ = "jungflor@gmail.com"
 __copyright__ = "Copyright (C) 2013-16, Florian JUNG"
 __license__ = "MIT"
-__version__ = "0.2.14b0"
-__date__ = "2016-03-02"
+__version__ = "0.3.0a0"
+__date__ = "2016-03-08"
 
 import logging
 
-from .logable import Logable, ModuleLogable
 from .loadable import Loadable, loadJSON, saveJSON, loadJSONFile, saveJSONFile
-from .webscraper import WebScraper, \
-    WEBConnectException, WEBFileException, WEBParameterException
+from .logable import Logable, ModuleLogable
 from .runable import StartStopable, Startable, Stopable, StartException
 
 
 logger = logging.getLogger(__name__)
-__all__ = ["logable", "loadable", "webscraper", "runable"]
+__all__ = ["logable", "loadable", "runable"]
