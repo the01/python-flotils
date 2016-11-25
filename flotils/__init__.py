20f54ffb80e18e9e8d1bc86e92eb40b530bb5151
d01 <jungflor@gmail.com>
2016-11-25 06:13:43 +0100

    Update version

--- flotils/__init__.py
+++ flotils/__init__.py
@@ -7,8 +7,8 @@ __author__ = "the01"
 __email__ = "jungflor@gmail.com"
 __copyright__ = "Copyright (C) 2013-16, Florian JUNG"
 __license__ = "MIT"
-__version__ = "0.3.2a0"
-__date__ = "2016-08-14"
+__version__ = "0.3.2b0"
+__date__ = "2016-11-25"
 
 import logging
 
